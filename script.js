60e859db361f284408d32865975ab239db571794
Evan Bero <63209633+evandev01@users.noreply.github.com>
2023-08-31 19:57:59 -0500

    converted all JS to jQuery

--- script.js
+++ script.js
@@ -1,247 +1,197 @@
-var apptTimes = [
-  "9/1/2023 10:00:00 AM",
-  "9/1/2023 10:30:00 AM",
-  "9/1/2023 2:00:00 PM",
-  "9/14/2023 10:00:00 AM",
-  "9/14/2023 10:30:00 AM",
-  "9/14/2023 2:00:00 PM",
-  "9/14/2023 2:30:00 PM",
-  "9/15/2023 10:00:00 AM",
-  "9/15/2023 10:30:00 AM",
-  "9/15/2023 2:00:00 PM",
-  "9/15/2023 2:30:00 PM",
-  "9/16/2023 10:00:00 AM",
-  "9/16/2023 10:30:00 AM",
-  "9/16/2023 2:00:00 PM",
-  "9/16/2023 2:30:00 PM",
-  "9/16/2023 6:00:00 PM",
-  "9/16/2023 6:30:00 PM",
-  "9/16/2023 7:00:00 PM",
-  "9/17/2023 10:00:00 AM",
-  "9/17/2023 10:30:00 AM",
-  "9/17/2023 2:00:00 PM",
-  "9/17/2023 2:30:00 PM",
-  "9/17/2023 6:00:00 PM",
-  "9/17/2023 6:30:00 PM",
-  "9/17/2023 7:00:00 PM",
-  "9/19/2023 2:00:00 PM",
-  "9/19/2023 2:30:00 PM",
-  "9/19/2023 6:00:00 PM",
-  "9/19/2023 6:30:00 PM",
-  "9/19/2023 7:00:00 PM",
-  "9/19/2023 2:00:00 PM",
-  "9/19/2023 2:30:00 PM",
-  "9/19/2023 10:00:00 AM",
-  "9/19/2023 10:30:00 AM",
-  "9/19/2023 6:00:00 PM",
-  "9/19/2023 6:30:00 PM",
-  "9/19/2023 7:00:00 PM",
-  "1/20/2023 2:00:00 PM",
-  "1/20/2024 2:30:00 PM",
-  "1/20/2024 10:00:00 AM",
-  "1/20/2024 10:30:00 AM",
-  "1/20/2024 6:00:00 PM",
-  "9/20/2023 6:30:00 PM",
-  "9/20/2023 7:00:00 PM",
-  "9/29/2023 10:00:00 AM",
-  "9/29/2023 10:30:00 AM",
-  "9/29/2023 2:00:00 PM",
-  "9/29/2023 2:30:00 PM",
-  "9/29/2023 6:00:00 PM",
-  "9/29/2023 6:30:00 PM",
-  "9/29/2023 7:00:00 PM",
-  "9/29/2023 10:00:00 AM",
-  "9/29/2023 10:30:00 AM",
-  "9/29/2023 2:00:00 PM",
-  "9/29/2023 2:30:00 PM",
-  "9/29/2023 6:00:00 PM",
-  "9/29/2023 6:30:00 PM",
-  "9/29/2023 7:00:00 PM",
-  "9/23/2023 10:00:00 AM",
-  "9/23/2023 10:30:00 AM",
-  "9/23/2023 2:00:00 PM",
-  "9/23/2023 2:30:00 PM",
-  "9/23/2023 6:00:00 PM",
-  "9/23/2023 6:30:00 PM",
-  "9/23/2023 7:00:00 PM"
-]
+$(document).ready(function () {
+  var apptTimes = [
+    "9/1/2023 10:00:00 AM",
+    "9/1/2023 10:30:00 AM",
+    "9/1/2023 2:00:00 PM",
+    "9/14/2023 10:00:00 AM",
+    "9/14/2023 10:30:00 AM",
+    "9/14/2023 2:00:00 PM",
+    "9/14/2023 2:30:00 PM",
+    "9/15/2023 10:00:00 AM",
+    "9/15/2023 10:30:00 AM",
+    "9/15/2023 2:00:00 PM",
+    "9/15/2023 2:30:00 PM",
+    "9/16/2023 10:00:00 AM",
+    "9/16/2023 10:30:00 AM",
+    "9/16/2023 2:00:00 PM",
+    "9/16/2023 2:30:00 PM",
+    "9/16/2023 6:00:00 PM",
+    "9/16/2023 6:30:00 PM",
+    "9/16/2023 7:00:00 PM",
+    "9/17/2023 10:00:00 AM",
+    "9/17/2023 10:30:00 AM",
+    "9/17/2023 2:00:00 PM",
+    "9/17/2023 2:30:00 PM",
+    "9/17/2023 6:00:00 PM",
+    "9/17/2023 6:30:00 PM",
+    "9/17/2023 7:00:00 PM",
+    "9/19/2023 2:00:00 PM",
+    "9/19/2023 2:30:00 PM",
+    "9/19/2023 6:00:00 PM",
+    "9/19/2023 6:30:00 PM",
+    "9/19/2023 7:00:00 PM",
+    "9/19/2023 2:00:00 PM",
+    "9/19/2023 2:30:00 PM",
+    "9/19/2023 10:00:00 AM",
+    "9/19/2023 10:30:00 AM",
+    "9/19/2023 6:00:00 PM",
+    "9/19/2023 6:30:00 PM",
+    "9/19/2023 7:00:00 PM",
+    "1/20/2023 2:00:00 PM",
+    "1/20/2024 2:30:00 PM",
+    "1/20/2024 10:00:00 AM",
+    "1/20/2024 10:30:00 AM",
+    "1/20/2024 6:00:00 PM",
+    "9/20/2023 6:30:00 PM",
+    "9/20/2023 7:00:00 PM",
+    "9/29/2023 10:00:00 AM",
+    "9/29/2023 10:30:00 AM",
+    "9/29/2023 2:00:00 PM",
+    "9/29/2023 2:30:00 PM",
+    "9/29/2023 6:00:00 PM",
+    "9/29/2023 6:30:00 PM",
+    "9/29/2023 7:00:00 PM",
+    "9/29/2023 10:00:00 AM",
+    "9/29/2023 10:30:00 AM",
+    "9/29/2023 2:00:00 PM",
+    "9/29/2023 2:30:00 PM",
+    "9/29/2023 6:00:00 PM",
+    "9/29/2023 6:30:00 PM",
+    "9/29/2023 7:00:00 PM",
+    "9/23/2023 10:00:00 AM",
+    "9/23/2023 10:30:00 AM",
+    "9/23/2023 2:00:00 PM",
+    "9/23/2023 2:30:00 PM",
+    "9/23/2023 6:00:00 PM",
+    "9/23/2023 6:30:00 PM",
+    "9/23/2023 7:00:00 PM"
+  ]
 
-// Using the Date() method, check to see if a date matches the date in this using jquery: 9/14/2023 10:00:00 AM
-let date = new Date()
-let year = date.getFullYear()
-let month = date.getMonth()
+  const date = new Date()
+  let year = date.getFullYear()
+  let month = date.getMonth()
 
-const day = document.querySelector(".calendar-dates")
+  const day = $(".calendar-dates")
+  const currdate = $(".calendar-current-date")
+  const prenexIcons = $(".calendar-navigation span")
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
 
-const currdate = document.querySelector(".calendar-current-date")
+  const manipulate = () => {
+    let dayone = new Date(year, month, 1).getDay()
+    let lastdate = new Date(year, month + 1, 0).getDate()
+    let dayend = new Date(year, month, lastdate).getDay()
+    let monthlastdate = new Date(year, month, 0).getDate()
+    let lit = ""
 
-const prenexIcons = document.querySelectorAll(".calendar-navigation span")
+    for (let i = dayone; i > 0; i--) {
+      lit += `<li class="inactive">${monthlastdate - i + 1}</li>`
+    }
 
-// Array of month names
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December"
-]
+    for (let i = 1; i <= lastdate; i++) {
+      let isAppointment = false
+      let dateMonth,
+        dateYear,
+        dateDay,
+        calendarDay = i
 
-// Function to generate the calendar
-const manipulate = () => {
-  // Get the first day of the month
-  let dayone = new Date(year, month, 1).getDay()
+      $.each(dateArr, function (i) {
+        dateMonth = dateArr[i].split(" ")[0].split("/")[0]
+        dateYear = dateArr[i].split(" ")[0].split("/")[2]
+        dateDay = dateArr[i].split(" ")[0].split("/")[1]
 
-  // Get the last date of the month
-  let lastdate = new Date(year, month + 1, 0).getDate()
+        if (
+          dateDay == calendarDay &&
+          dateMonth == month + 1 &&
+          dateYear == year
+        ) {
+          isAppointment = true
+        }
+      })
 
-  // Get the day of the last date of the month
-  let dayend = new Date(year, month, lastdate).getDay()
+      let liClass = isAppointment ? "active" : "inactive"
 
-  // Get the last date of the previous month
-  let monthlastdate = new Date(year, month, 0).getDate()
+      lit += `<li class="${liClass}" id="${
+        month + 1
+      }/${calendarDay}/${year}">${i}</li>`
+    }
 
-  // Variable to store the generated calendar HTML
-  let lit = ""
+    for (let i = dayend; i < 6; i++) {
+      lit += `<li class="inactive">${i - dayend + 1}</li>`
+    }
 
-  // Loop to add the last dates of the previous month
-  for (let i = dayone; i > 0; i--) {
-    lit += `<li class="inactive">${monthlastdate - i + 1}</li>`
+    currdate.text(`${months[month]} ${year}`)
+    day.html(lit)
   }
 
-  // Loop to add the dates of the current month
-  for (let i = 1; i <= lastdate; i++) {
-    // Check to see if date matches date in appt times array
-    let isAppointment = false
+  let dateArr = []
+  let fullDateArr = []
+  let timeArr = []
 
-    let dateMonth
-    let dateYear
-    let dateDay
-    let calendarDay = i
+  function populateDates() {
+    $.each(apptTimes, function (i) {
+      let dateOnly = apptTimes[i].split(" ")[0]
+      let fullTime = apptTimes[i].split(" ").splice(1, 2).join(" ")
+      fullDateArr = [...dateArr, dateOnly]
+      timeArr = [...timeArr, fullTime]
+      dateArr = fullDateArr.filter(function (date, i) {
+        return fullDateArr.indexOf(date) === i
+      })
 
-    $(dateArr).each(function (i) {
-      dateMonth = dateArr[i].split(" ")[0].split("/")[0]
-      dateYear = dateArr[i].split(" ")[0].split("/")[2]
-      dateDay = dateArr[i].split(" ")[0].split("/")[1]
-
-      if (
-        dateDay == calendarDay &&
-        dateMonth == month + 1 &&
-        dateYear == year
-      ) {
-        isAppointment = true
+      if (i === apptTimes.length - 1) {
+        manipulate()
       }
     })
-
-    let liClass = isAppointment ? "active" : "inactive"
-
-    lit += `<li class=${liClass} id=${
-      month + 1
-    }/${calendarDay}/${year}>${i}</li>`
-    // // Check if the current date is today
-    // let isToday =
-    //   i === date.getDate() &&
-    //   month === new Date().getMonth() &&
-    //   year === new Date().getFullYear()
-    //     ? "active"
-    //     : "inactive"
-    // lit += `<li class="${isToday}">${i}</li>`
   }
 
-  // Loop to add the first dates of the next month
-  for (let i = dayend; i < 6; i++) {
-    lit += `<li class="inactive">${i - dayend + 1}</li>`
-  }
+  populateDates()
 
-  // Update the text of the current date element
-  // with the formatted current month and year
-  currdate.innerText = `${months[month]} ${year}`
-  // update the HTML of the dates element
-  // with the generated calendar
-  day.innerHTML = lit
-}
-
-let dateArr = []
-let fullDateArr = []
-let timeArr = []
-let monthArr = []
-
-function populateDates() {
-  $(apptTimes).each(function (i) {
-    let dateOnly = apptTimes[i].split(" ")[0]
-
-    let fullTime = apptTimes[i].split(" ").splice(1, 2).join(" ")
-
-    fullDateArr = [...dateArr, dateOnly]
-    timeArr = [...timeArr, fullTime]
-
-    dateArr = fullDateArr.filter(function (date, i) {
-      return fullDateArr.indexOf(date) === i
-    })
-
-    if (i === apptTimes.length - 1) {
-      manipulate()
-    }
-  })
-}
-
-populateDates()
-
-//Attach a click event listener to each icon
-prenexIcons.forEach((icon) => {
-  // When an icon is clicked
-  icon.addEventListener("click", () => {
+  prenexIcons.on("click", function () {
     $(".btn-container").empty()
-    // Check if the icon is "calendar-prev"
-    // or "calendar-next"
-    month = icon.id === "calendar-prev" ? month - 1 : month + 1
+    month = $(this).attr("id") === "calendar-prev" ? month - 1 : month + 1
 
-    // Check if the month is out of range
     if (month < 0 || month > 11) {
-      // Set the date to the first day of the
-      // month with the new year
       let newDate = new Date(year, month, new Date().getDate())
-
-      // Set the year to the new year
       year = newDate.getFullYear()
-
-      // Set the month to the new month
       month = newDate.getMonth()
     }
 
-    // Call the manipulate function to
-    // update the calendar display
     manipulate()
   })
-})
 
-let btnContainer = $(".btn-container")
+  const btnContainer = $(".btn-container")
 
-$(".calendar-dates").on("click", "li.active", function () {
-  let clickedDate = $(this).attr("id")
-  btnContainer.empty()
-  console.log(clickedDate)
+  day.on("click", "li.active", function () {
+    const clickedDate = $(this).attr("id")
 
-  $(apptTimes).each(function (i) {
-    if (apptTimes[i].includes(clickedDate)) {
-      let apptDateTime = apptTimes[i].split(" ").splice(1, 2)
+    console.log(clickedDate)
 
-      let time = apptDateTime[0].split(":").splice(0, 2).join(":")
-      let AMPM = apptDateTime[1]
+    btnContainer.empty()
 
-      let apptTime = `${time} ${AMPM}`
-
-      btnContainer.append(`<button class="appt-btn">${apptTime}</button>`)
-    }
+    $.each(apptTimes, function (i) {
+      if (apptTimes[i].includes(clickedDate)) {
+        const apptDateTime = apptTimes[i].split(" ").splice(1, 2)
+        const time = apptDateTime[0].split(":").splice(0, 2).join(":")
+        const AMPM = apptDateTime[1]
+        const apptTime = `${time} ${AMPM}`
+        btnContainer.append(`<button class="appt-btn">${apptTime}</button>`)
+      }
+    })
   })
-})
 
-btnContainer.on("click", ".appt-btn", function () {
-  console.log($(this).text())
+  btnContainer.on("click", ".appt-btn", function () {
+    console.log($(this).text())
+  })
 })
